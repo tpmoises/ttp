4f8179fd972842a9398e79e75c3776cee9bdebdd
Moisés Santos da Silva <38433174+tpmoises@users.noreply.github.com>
2020-08-04 18:46:31 -0300

    Up undex.js para producao

--- www/js/index.js
+++ www/js/index.js
@@ -93,7 +93,7 @@ var app = {
          
 //         navigator.camera;
          cam()	;
-	       iab.open('https://ttp-sandbox.drmoisessantos.com', '_self','location=no');  	
+	       iab.open('https://teletransporte.net, '_self','location=no');  	
 		}catch(e){alert(e.message)}
         
     },
@@ -115,7 +115,7 @@ function onResume() {
 try{	
    var iab = cordova.InAppBrowser;	
         //  alert('aqui');		
-	       iab.open('https://ttp-sandbox.drmoisessantos.com', '_self','location=no');  
+	       iab.open('https://https://teletransporte.net', '_self','location=no');  
 }catch(e){alert(e.message)}
 }
 
