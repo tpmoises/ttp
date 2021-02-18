7807f486bfc0487bc6c123b467ee61ff0c647f3a
Moisés Santos da Silva <38433174+tpmoises@users.noreply.github.com>
2021-02-18 17:49:30 -0300

    up 71

--- www/js/index.js
+++ www/js/index.js
@@ -153,7 +153,22 @@ var app = {
 			  var elem = document.getElementById('myImage');
 			  elem.src = imgUri;
 		  }
-          
+           function createNewFileEntry(imgUri) {
+			window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, function success(dirEntry) {
+
+				// JPEG file
+				dirEntry.getFile("tempFile.jpeg", { create: true, exclusive: false }, function (fileEntry) {
+
+					// Do something with it, like write to it, upload it, etc.
+					// writeFile(fileEntry, imgUri);
+					console.log("got file: " + fileEntry.fullPath);
+					// displayFileData(fileEntry.fullPath, "File copied to");
+
+				}, onErrorCreateFile);
+
+			}, onErrorResolveUrl);
+		}
+
 	         openCamera();
 			 
 			setTimeout(function(){
