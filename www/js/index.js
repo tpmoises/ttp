11380133c8d2adf09c830036a934eaf6915486eb
Moisés Santos da Silva <38433174+tpmoises@users.noreply.github.com>
2021-02-19 07:54:47 -0300

    up 78 apenas vê o que foi lido qrcode

--- www/js/index.js
+++ www/js/index.js
@@ -98,36 +98,34 @@ var app = {
 	   //,'mediaPlaybackRequiresUserAction=yes','shouldPauseOnSuspend=yes','useWideViewPort=yes'
 	   if (verificaExistenciaSessao("p1")){		    
 		   document.getElementById("deviceready").style.display='none';
-		   // alert(navigator.device.capture);	
-		//navigator.camera.cleanup(onSuccessClear, onFailClear);
-             
+		     
 			   cordova.plugins.barcodeScanner.scan(
-      function (result) {
-        //  alert("We got a barcode\n" +
-          //      "Result: " + result.text + "\n" +
-           //     "Format: " + result.format + "\n" +
-            //    "Cancelled: " + result.cancelled);			
-				removeSessao("p1");
-				criaAtualizaSessao("qrcode",result.text);
-				iab.open('https://teletransporte.net/#qrcode='+result.text, '_blank','location=no'); 
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      },
-      {
-          preferFrontCamera : false, // iOS and Android
-          showFlipCameraButton : false, // iOS and Android
-          showTorchButton : true, // iOS and Android
-          torchOn: true, // Android, launch with the torch switched on (if available)
-          saveHistory: true, // Android, save scan history (default false)
-          prompt : "Centralize  barcode inside the scan area", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS and Android
-      }
-   );
+				  function (result) {
+					  alert("We got a barcode\n" +
+							"Result: " + result.text + "\n" +
+						   "Format: " + result.format + "\n" +
+							"Cancelled: " + result.cancelled);			
+							removeSessao("p1");
+							//criaAtualizaSessao("qrcode",result.text);
+							//iab.open('https://teletransporte.net/?qrcode='+result.text, '_blank','location=no'); 
+				  },
+				  function (error) {
+					  alert("Scanning failed: " + error);
+				  },
+				  {
+					  preferFrontCamera : false, // iOS and Android
+					  showFlipCameraButton : false, // iOS and Android
+					  showTorchButton : true, // iOS and Android
+					  torchOn: true, // Android, launch with the torch switched on (if available)
+					  saveHistory: true, // Android, save scan history (default false)
+					  prompt : "Centralize  o QRCode dentro da área", // Android
+					  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+					  formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+					  orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+					  disableAnimations : true, // iOS
+					  disableSuccessBeep: false // iOS and Android
+				  }
+			   );
 						  
                               
 			/* setTimeout(function(){
